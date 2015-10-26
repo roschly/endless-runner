f4e15400430ee187eb00801f37848d58841b5da4
roschly <roschly@gmail.com>
2015-10-26 18:24:13 +0100

    Initial Commit

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject thePlatform;
+	public Transform generationPoint;
+	public float distanceBetweenMin;
+	public float distanceBetweenMax;
+	public ObjectPooler theObjectPool;
+	public GameObject[] thePlatforms;
+
+	private float platformWidth;
+	private float distanceBetween;
+	private float[] platformWidths;
+	private float lastPlacedPlatformWidth;
+
+
+	private int platformSelector;
+
+	// Use this for initialization
+	void Start () {
+
+		platformWidths = new float[thePlatforms.Length];
+
+		for (int i = 0; i < platformWidths.Length; i++) {
+			platformWidths[i] = thePlatforms[i].GetComponent<BoxCollider2D>().size.x;
+		}
+
+		lastPlacedPlatformWidth = 7;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < generationPoint.position.x) {
+			distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+
+			platformSelector = Random.Range(0, thePlatforms.Length);
+
+			float distanceToMove = platformWidths[platformSelector]/2 + lastPlacedPlatformWidth/2 + distanceBetween;
+
+			transform.position = new Vector3(transform.position.x + distanceToMove,
+			                                 transform.position.y, transform.position.z);
+
+
+			Instantiate(thePlatforms[platformSelector], transform.position, transform.rotation);
+			//GameObject newPlatform = theObjectPool.getPooledObject();
+			//newPlatform.transform.position = transform.position;
+			//newPlatform.transform.rotation = transform.rotation;
+			//newPlatform.SetActive(true);
+
+			lastPlacedPlatformWidth = platformWidths[platformSelector];
+		}
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+	public bool grounded;
+	public LayerMask WhatIsGround;
+
+	private Rigidbody2D myRigidbody;
+	private Collider2D myCollider;
+	private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> ();
+		myCollider = GetComponent<Collider2D> ();
+		myAnimator = GetComponent<Animator> ();
+		grounded = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+		grounded = Physics2D.IsTouchingLayers (myCollider, WhatIsGround);
+		
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) {
+			myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+		}
+
+		myAnimator.SetFloat("moveSpeed", moveSpeed);
+		myAnimator.SetBool("grounded", grounded);
+	
+	}
+}
